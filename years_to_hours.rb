83b4e93572cef4edbd1a41e3cc9cfabfd355f1fe
nmvilla15 <niki.villalpando@gmail.com>
2021-03-30 15:43:30 -0500

    completed assignment

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -9,12 +9,12 @@ puts "That's #{hours} hours."
 puts "Please enter a number of decades"
 decades = gets.chomp
 decades = decades.to_i
-minutes = decades * 10 * 365 * 24 * 60
+minutes = decades * 10 * 365.25 * 24 * 60
 puts "That's equal to #{minutes} minutes."
 
 #part three, user age
 puts "Please enter your age"
 age = gets.chomp
 age = age.to_i
-seconds = age * 365 * 24 * 60 * 60
+seconds = age * 365.25 * 24 * 60 * 60 #since i'm not asking for year born, using 365.25 to account for leap year, same for part 2
 puts "You are #{seconds} seconds old."
